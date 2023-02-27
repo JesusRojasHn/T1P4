c894862e06c1f7fa5c708d9316f7e6775b27636f
lolgi <lolgi@LAPTOP-J3FV9M1B>
2023-02-26 20:54:07 -0600

    Add project files.

---
+++ T1P4/T1P4/Main.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T1P4.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace T1P4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Main : ContentPage
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSuma_Clicked(object sender, EventArgs e)
+        {
+            double nr1 = Convert.ToDouble(txtN1.Text);
+            double nr2 = Convert.ToDouble(txtN2.Text);
+            double suma = nr1 + nr2;
+           
+            var op = new Operations
+            {
+                N1 = nr1,
+                N2 = nr2,
+                OP = suma
+                
+            };
+
+            var res = new Result();
+            res.BindingContext = op;
+            Navigation.PushAsync(res);
+        }
+
+        private void btnResta_Clicked(object sender, EventArgs e)
+        {
+            double nr1 = Convert.ToDouble(txtN1.Text);
+            double nr2 = Convert.ToDouble(txtN2.Text);
+            double resta = nr1 - nr2;
+
+            var op = new Operations
+            {
+                N1 = nr1,
+                N2 = nr2,
+                OP = resta
+
+            };
+
+            var res = new Result();
+            res.BindingContext = op;
+            Navigation.PushAsync(res);
+        }
+
+        private void btnDividir_Clicked(object sender, EventArgs e)
+        {
+            double nr1 = Convert.ToDouble(txtN1.Text);
+            double nr2 = Convert.ToDouble(txtN2.Text);
+            double dividir = nr1 / nr2;
+
+            var op = new Operations
+            {
+                N1 = nr1,
+                N2 = nr2,
+                OP = dividir
+
+            };
+
+            var res = new Result();
+            res.BindingContext = op;
+            Navigation.PushAsync(res);
+        }
+
+        private void btnMultiplicar_Clicked(object sender, EventArgs e)
+        {
+            double nr1 = Convert.ToDouble(txtN1.Text);
+            double nr2 = Convert.ToDouble(txtN2.Text);
+            double multi = nr1 * nr2;
+
+            var op = new Operations
+            {
+                N1 = nr1,
+                N2 = nr2,
+                OP = multi
+
+            };
+
+            var res = new Result();
+            res.BindingContext = op;
+            Navigation.PushAsync(res);
+        }
+
+
+   
+    }
+}
---
+++ T1P4/T1P4/Models/Operations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T1P4.Models
+{
+    public class Operations
+    {
+        public double N1 { get; set; }
+        public double N2 { get; set; }
+        public double OP { get; set; }
+    }
+}
